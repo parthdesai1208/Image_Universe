767532eacd95c154e4947dc4b31b9b9ca192b55b
parthdesai1208 <dparth78@gmail.com>
2020-01-22 11:41:41 +0530

    add navigation & screens(ImageList & Detail)

---
+++ app/src/main/java/com/parthdesai1208/imageuniverse/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.parthdesai1208.imageuniverse
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+}
--- app/src/main/java/com/parthdesai1208/imageuniverse/MainActivity.kt
+++ app/src/main/java/com/parthdesai1208/imageuniverse/MainActivity.kt
@@ -2,11 +2,21 @@ package com.parthdesai1208.imageuniverse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this,
+            R.id.nav_fragment
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
 }
